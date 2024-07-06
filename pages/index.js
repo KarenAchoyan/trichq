f4b553b6c4eadf805c72233858d7c830a1f53f0c
Rafo <relax1199@mail.ru>
2024-07-06 16:30:17 +0400

    Changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import {Inter} from "next/font/google";
 import styles from "../styles/Home.module.css";
 
 import Blog from "../components/blog/blog";
@@ -18,7 +17,6 @@ import {getSlides} from "../store/slides/actions";
 import {getGalleries} from "../store/gallery/actions";
 
 
-const inter = Inter({subsets: ["latin"]});
 
 export default function Home() {
     const blogs = useSelector((state) => state.blog.blogs);
